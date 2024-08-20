d083e764ad0c46247993c684f96111ac52369586
Ignacio <ignaciotejada87@gmail.com>
2024-08-20 21:31:47 +0200

    Upload 20/08/2024 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from 'src/environments/environment';
+
+
 
 import { APP_ROUTING } from './app-routing.module';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './componentes/home/home.component';
@@ -12,6 +17,7 @@ import { AuthModule, provideAuth0 } from '@auth0/auth0-angular';
 import { PerfilComponent } from './componentes/perfil/perfil.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './componentes/footer/footer.component';
+
 
 
 @NgModule({
@@ -27,6 +33,9 @@ import { FooterComponent } from './componentes/footer/footer.component';
     BrowserModule,
     HttpClientModule,
     APP_ROUTING,
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
     AuthModule.forRoot({
       domain: "dev-12csjvjuyvgnl25b.eu.auth0.com",
       clientId: "WKxIK678zaYDuVY4iUntwfrsCxHw3bdn",
